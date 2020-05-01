3030205b9f9d4f566d97b969c0039a227f615e0b
Denis Rafi <62852838+DenisError404@users.noreply.github.com>
2020-05-01 01:34:58 +0100

    Add files via upload

---
+++ Program, which creates square matrices.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Solution
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("By Denis Rafi");
+            Console.WriteLine("-----------------");
+            Console.WriteLine($"{DateTime.Now:d}");
+            Console.Write("Enter height: ");
+            int y = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter width: ");
+            int x = Int32.Parse(Console.ReadLine());
+            int a = 0;
+            for (int i = 1; i <= y; i++)
+            {
+                Console.Write("{0} ", i);
+                a += i;
+                for (int j = 1; j < x; j++)
+                {
+                    a += y;
+                    Console.Write("{0} ", a);
+                }
+                a = 0;
+                Console.WriteLine();
+            }
+        }
+    }
+}
